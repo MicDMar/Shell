170c25ca0b2e81602362cf4fd10c7d4249c1568b
MicDMar <martmic2@bvu.edu>
2019-09-18 14:46:40 -0500

    Added input output redirection.

--- shell.c
+++ shell.c
@@ -14,8 +14,10 @@
 char inputBuf[1024];
 char cwd[1024];
 char *args[1024];
+char input_file[1024];
+char output_file[1024];
 char *usr;
-
+int append = 0;
 
 /* This function prints the output in a General format
    which will be handy for debugging and for basic
@@ -127,69 +129,159 @@ void change_dir(char *dir){
 }
 
 
+void redirect(){
+  
+  char *ch = inputBuf;
+  int space = 1;
+  int first = 1;
+
+  while(*ch != '\0'){
+    if(*ch == '<'){
+      if(first == 1){
+        *ch = '\0';
+        first = 0;
+      }
+      ch++;
+      while(space == 1){
+        if(*ch == ' '){
+          ch++;
+        } else {
+          space = 0;
+        }
+      }
+      char *in = input_file;
+      while(*ch != ' ' && *ch != '\0'){
+        *in = *ch;
+        ch++;
+        in++;
+      }
+      *in = '\0';
+      space = 1;
+    }
+
+    if(*ch == '>'){
+      if(first == 1){
+        *ch = '\0';
+        first = 0;
+      }
+      ch++;
+      if(*ch == '>'){
+        append = 1;
+        ch++;
+      }
+      while(space == 1){
+        if(*ch == ' '){
+          ch++;
+        } else {
+          space = 0;
+        }
+      }
+      char *out = output_file;
+      while(*ch != ' ' && *ch != '\0'){
+        *out = *ch;
+        ch++;
+        out++;
+      }
+      *out = '\0';
+      space = 1; 
+    }
+
+    ch++;
+  }
+}
+
+void erase_files(){
+  
+  char *eraser = input_file;
+  *eraser = '\0';
+  eraser = output_file;
+  *eraser = '\0';
+  append = 0;
+}
+
+void execute_command(){
+    
+  int pid = fork(); 
+  if(pid == 0){
+
+    if(strlen(input_file) != 0){
+      close(0);
+      open(input_file, O_RDONLY | O_CREAT, 0666);
+    }
+
+    if(strlen(output_file) != 0){
+      close(1);
+      if(append == 1){
+        open(output_file, O_APPEND | O_CREAT, 0666);
+      } else {
+        open(output_file, O_WRONLY | O_CREAT, 0666);
+      }
+    }
+
+    execvp(args[0], args);
+    exit(1);
+  } else {
+    int exitCode;
+    while( wait(&exitCode) != pid);
+  }
+}
+
 /* This function parses input from the user, creating
    a list of arguments with the first of those being the
    command the user wants to execute. */
 int parse_input(){
+
+  redirect();
+
+  char *ch = inputBuf;
+
+  int x = 0;
+  int first_space = 1;
+  int first_char = 1;
+
+  while(*ch != '\0'){ 
+    if(*ch == ' '){
+      if(first_space){
+        *ch = '\0';
+        first_space = 0;
+        first_char = 1;
+      }
+    } else {
+      if(first_char){
+        args[x] = ch;
+        x++;
+        first_space = 1;
+        first_char = 0;
+      }
+    }
+    ch++;
+  }
+
+  args[x] = NULL;
+
+  // If the user specified command is debugargs...
+  if(strcmp("debugargs", args[0]) == 0){
+     debug_args();
+  }
   
-  // Check to see if the user wants to exit the shell.
-  if(strcmp("exit", inputBuf) == 0){
+  // If the user specified command is cd...
+  if(strcmp("cd", args[0]) == 0){
+     change_dir(args[1]);
+  }
+
+  // If the user specified command is pwd...
+  if(strcmp("pwd", args[0]) == 0){
+    print(cwd);
+  }
+
+  // If the user specified command is exit
+  // return 1 so the main function can break.
+  if(strcmp("exit", args[0]) == 0){
     return 1;
   }
-  
-  // The user doesn't want to exit the shell <Command inputted>
-  else {
 
-    char *ch = inputBuf;
-
-    int x = 0;
-    int first_space = 1;
-    int first_char = 1;
-
-    while(*ch != '\0'){ 
-      if(*ch == ' '){
-        if(first_space){
-          *ch = '\0';
-          first_space = 0;
-          first_char = 1;
-        }
-      } else {
-        if(first_char){
-          args[x] = ch;
-          x++;
-          first_space = 1;
-          first_char = 0;
-        }
-      }
-      ch++;
-    }
-
-    args[x] = NULL;
-
-    // If the user specified command is debugargs...
-    if(strcmp("debugargs", args[0]) == 0){
-       debug_args();
-    }
-    
-    // If the user specified command is cd...
-    if(strcmp("cd", args[0]) == 0){
-       change_dir(args[1]);
-    }
-
-    // If the user specified command is pwd...
-    if(strcmp("pwd", args[0]) == 0){
-      print(cwd);
-    }
-
-    // If the user specified command is exit
-    // return 1 so the main function can break.
-    if(strcmp("exit", args[0]) == 0){
-      return 1;
-    }
-
-    // Everything went well!
-    return 0;
-  }
+  // Everything went well!
+  return 0;
 }
 
 
@@ -211,19 +303,16 @@ int main(int argc, char **argv){
 
     // Parse the input given and determine what to do with it.
     if(parse_input() == 1){
-      break; 
-    } 
-   
-    int pid = fork(); 
-    if(pid == 0){
-      execvp(args[0], args);
-    } else {
-      int exitCode;
-      while( wait(&exitCode) != pid);
+      break;
     }
 
+    execute_command();
+
+    erase_files();
+    
     //DEBUG OUTPUT 
     //print(inputBuf);
+
   }
 
   print("\nGoodbye.");
